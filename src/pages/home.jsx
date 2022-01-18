79c13496a41b82d5454af9825d2c91fb8a777525
Chow Jing Lun <chowjl94@gmail.com>
2022-01-18 16:07:35 +0800

    version2 branch

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,29 +12,35 @@ export const Home=() =>{
     return (
     <div className='bg-wood bg-no-repeat bg-left' >
         <div id = 'home-page' className='bg-transparent' > 
-            <p className='flex justify-center'>what should i put here</p>       
+        <header className='flex justify-center'>Mutou</header>
+            <div><br></br></div>      
         </div>
 
         <div id = 'products' className='flex flex-col lg:relative'>
 
             <header className='flex justify-center'>My Products</header>
             <div className= 'lg:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2'>
+                
                 <div className = 'lg-block lg:inset-0 lg:right-0 lg:w-1/2 grid grid-cols-2 grid-rows-2 px-10 gap-4'>           
-                        <img alt='' src='https://www.facebook.com/images/fb_icon_325x325.png' ></img>
-                        <img alt='' src='https://www.facebook.com/images/fb_icon_325x325.png' ></img>
-                        <img alt='' src='https://www.facebook.com/images/fb_icon_325x325.png' ></img>
-                        <img alt='' src='https://www.facebook.com/images/fb_icon_325x325.png' ></img>
+
                 </div>
             </div>  
+            <br></br>
         </div>
+        
 
         <div id ='about-me'>
-            <p className='flex justify-center'>More information about me</p>  
+            <header className='flex justify-center'>About me</header>
+            <br></br>
         </div>
-
+        
         <div id ='contact-me'>
-            <p className='flex justify-center'>Contact information</p> 
-
+        <header className='flex justify-center'>Contact Info</header>
+        <br></br>  
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br> 
         </div>
 
         <div id = 'connect-with-me' className='inset-x-0 bottom-0'>
